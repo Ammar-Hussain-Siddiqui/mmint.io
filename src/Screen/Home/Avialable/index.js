285144ea999490c9eaf42e776fb4afc8684f71a7
Ammarsiddique2 <soc.solutions.ammarsiddique@outlook.com>
2023-03-03 12:03:14 +0500

    first

---
+++ src/Screen/Home/Avialable/index.js
@@ -0,0 +1,532 @@
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import MacroOffIcon from "@mui/icons-material/MacroOff";
+import "./index.css";
+
+const Avialable = () => {
+  return (
+    <div>
+      {/* Avialble */}
+      <div
+        style={{
+          backgroundColor: "rgb(255, 184, 0)",
+          borderBottom: "2px solid white",
+        }}
+        id="we_Avialable"
+      >
+        <h1
+          style={{
+            textAlign: "center",
+            color: "black",
+            paddingTop: "30px",
+            paddingBottom: "30px",
+            fontFamily: "Times New Roman, Times, serif",
+          }}
+          data-aos="fade-up"
+          data-aos-duration="3000"
+        >
+          <b>We Are Avialable</b>{" "}
+        </h1>
+        <Container>
+          <Grid container className="avialble_main">
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={2.5} xs={3}>
+                  <MacroOffIcon style={{ margin: "15px", fontSize: "25px" }} />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={2.5} xs={3}>
+                  <MacroOffIcon style={{ margin: "15px", fontSize: "25px" }} />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={2.5} xs={3}>
+                  <MacroOffIcon style={{ margin: "15px", fontSize: "25px" }} />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid container className="avialble_main1">
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={3} xs={3}>
+                  <MacroOffIcon style={{ margin: "15px", fontSize: "25px" }} />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={3} xs={3}>
+                  <MacroOffIcon style={{ margin: "15px", fontSize: "25px" }} />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={3} xs={3}>
+                  <MacroOffIcon style={{ margin: "15px", fontSize: "25px" }} />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            className="avialble_main1"
+            style={{ paddingBottom: "40px" }}
+          >
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={3} xs={3}>
+                  <MacroOffIcon style={{ margin: "15px", fontSize: "25px" }} />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text"
+              id="avialable_text"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={3} xs={3}>
+                  <MacroOffIcon style={{ margin: "15px", fontSize: "25px" }} />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item lg={2.5} md={2.5} sm={3.3}>
+              {/* <Grid container>
+                <Grid item lg={1.5}>
+                  <MacroOffIcon style={{ margin: "15px", fontSize: "25px" }} />
+                </Grid>
+                <Grid item lg={5} className="avialable_p">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid> */}
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+      {/* COMMING SOON */}
+      <div style={{ backgroundColor: "rgb(255,138,0)" }}>
+        <h1
+          style={{
+            textAlign: "center",
+            color: "black",
+            paddingTop: "30px",
+            paddingBottom: "30px",
+            fontFamily: "Times New Roman, Times, serif",
+          }}
+          data-aos="fade-up"
+          data-aos-duration="3000"
+        >
+          <b>Coming Soon</b>{" "}
+        </h1>
+        <Container>
+          <Grid container className="avialble_main">
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text_comming"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={3} xs={3}>
+                  <MacroOffIcon
+                    style={{ margin: "15px", fontSize: "25px", color: "white" }}
+                  />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p_comming">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text_comming"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={3} xs={3}>
+                  <MacroOffIcon
+                    style={{ margin: "15px", fontSize: "25px", color: "white" }}
+                  />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p_comming">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text_comming"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={3} xs={3}>
+                  <MacroOffIcon
+                    style={{ margin: "15px", fontSize: "25px", color: "white" }}
+                  />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p_comming">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            className="avialble_main"
+            style={{ marginTop: "30px" }}
+          >
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text_comming"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={3} xs={3}>
+                  <MacroOffIcon
+                    style={{ margin: "15px", fontSize: "25px", color: "white" }}
+                  />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p_comming">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text_comming"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={3} xs={3}>
+                  <MacroOffIcon
+                    style={{ margin: "15px", fontSize: "25px", color: "white" }}
+                  />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p_comming">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text_comming"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={3} xs={3}>
+                  <MacroOffIcon
+                    style={{ margin: "15px", fontSize: "25px", color: "white" }}
+                  />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p_comming">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            className="avialble_main"
+            style={{ marginTop: "30px" }}
+          >
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text_comming"
+            >
+              <Grid container>
+                <Grid item lg={1.5} sm={3} xs={3}>
+                  <MacroOffIcon
+                    style={{ margin: "15px", fontSize: "25px", color: "white" }}
+                  />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p_comming">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              lg={2.5}
+              md={2.5}
+              sm={3.3}
+              xs={6}
+              className="avialable_text_comming"
+              id="avialable_text"
+            >
+              <Grid container>
+                <Grid item lg={1.5} md={2.5} sm={3} xs={3}>
+                  <MacroOffIcon
+                    style={{ margin: "15px", fontSize: "25px", color: "white" }}
+                  />
+                </Grid>
+                <Grid item lg={5} xs={5} className="avialable_p_comming">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid item lg={2.5} md={2.5} sm={3.3}>
+              {/* <Grid container>
+                <Grid item lg={1.5}>
+                  <MacroOffIcon
+                    style={{ margin: "15px", fontSize: "25px", color: "white" }}
+                  />
+                </Grid>
+                <Grid item lg={5} className="avialable_p_comming">
+                  <p>
+                    <b>KyberSwap</b>
+                  </p>
+                </Grid>
+              </Grid> */}
+            </Grid>
+          </Grid>
+        </Container>
+        <div
+          style={{
+            borderBottom: "1px solid rgb(54,54,54)",
+            marginBottom: "30px",
+            paddingTop: "30px",
+          }}
+        ></div>
+        <div style={{ paddingTop: "10px" }}></div>
+      </div>
+
+      {/* Subscribe */}
+
+      <div
+        style={{
+          backgroundColor: "rgb(54,54,54)",
+          borderBottom: "2px solid white",
+        }}
+      >
+        <h1
+          style={{
+            textAlign: "center",
+            color: "black",
+            paddingTop: "30px",
+            paddingBottom: "30px",
+            color: "white",
+            fontFamily: "Times New Roman, Times, serif",
+          }}
+          data-aos="fade-up"
+          data-aos-duration="3000"
+        >
+          <b>Subscribe Your Letter</b>
+        </h1>
+
+        <Container>
+          <Grid container>
+            <Grid item lg={4} md={4} sm={4}></Grid>
+            <Grid
+              item
+              lg={4}
+              md={4}
+              sm={4}
+              style={{ textAlign: "center", marginTop: "-30px" }}
+            >
+              <div style={{ borderBottom: "2px solid white" }}></div>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            style={{
+              justifyContent: "center",
+              marginTop: "20px",
+              marginBottom: "30px",
+            }}
+          >
+            <Grid item lg={6} md={6} sm={6}>
+              <Grid
+                container
+                style={{ backgroundColor: "gray", borderRadius: "8px" }}
+              >
+                <Grid item lg={8} md={8} sm={8}>
+                  <input
+                    type="text"
+                    style={{
+                      outline: "none",
+                      border: "none",
+                      backgroundColor: "transparent",
+                      padding: "10px",
+                      paddingLeft: "18px",
+                      paddingTop: "20px",
+                      cursor: "pointer",
+                    }}
+                    placeholder="Enter Your Email"
+                    className="input_email"
+                  />
+                </Grid>
+
+                <Grid item lg={2} md={1} sm={0.5}></Grid>
+
+                <Grid item lg={2} md={2} sm={2}>
+                  <Button
+                    style={{
+                      backgroundColor: "rgb(255,138,0)",
+                      color: "white",
+                      margin: "5px",
+                      borderRadius: "8px",
+                      marginTop: "10px",
+                    }}
+                  >
+                    <b>Submit</b>
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Container>
+        <div
+          style={{
+            borderBottom: "1px solid gray",
+            marginBottom: "30px",
+            paddingTop: "30px",
+          }}
+        ></div>
+        <div style={{ paddingTop: "10px" }}></div>
+      </div>
+    </div>
+  );
+};
+
+export default Avialable;
